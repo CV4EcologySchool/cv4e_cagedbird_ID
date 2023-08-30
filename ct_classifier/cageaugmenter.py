d4a7e036208b2e1c5695e0eb27f6c870f15b78be
sicily <sicilyfiennes@gmail.com>
2023-08-30 19:45:45 +0000

    still working on those annotations

--- ct_classifier/cageaugmenter.py
+++ ct_classifier/cageaugmenter.py
@@ -67,10 +67,7 @@ mask_color = random_color
 rotate_range = [-20, 20]  # Rotation range in degrees
 rotate = math.radians(random.uniform(*rotate_range))
 
-# Insert this code snippet in the appropriate place before creating the mask. This will allow you to generate random 
-# values for the bar radius, color, and rotation each time the script is run. Adjust the range values as needed for your desired level of randomness.
-
-#create mask
+# Create mask
 bar_pos_x = [int(i) for i in np.linspace(0, resolution[0], num_bars_x)]
 bar_pos_y = [int(i) for i in np.linspace(0, resolution[1], num_bars_y)]
 bar_locs_x = []
@@ -87,14 +84,14 @@ for i in range(resolution[0]):
 mask = cv2.blur(mask, kernel, cv2.BORDER_DEFAULT)
 mask = np.array(Image.fromarray(mask).rotate(rotate))
 
-#taking a center crop to remove the rotation artifacts
+# taking a center crop to remove the rotation artifacts
 plt.imshow(np.array(mask))
 crop_size = rotatedRectWithMaxArea(mask.shape[0], mask.shape[1], rotate)
 x_boundary = int((mask.shape[0]-crop_size[0])/2)
 y_boundary = int((mask.shape[1]-crop_size[1])/2)
 mask = mask[x_boundary:-x_boundary, y_boundary:-y_boundary]
 
-#resize and tile
+# resize and tile
 mask = cv2.resize(mask, im.size)
 np.expand_dims(mask, 2).shape
 np.tile(np.expand_dims(mask, 2), (1,1,3)).shape
@@ -111,10 +108,5 @@ plt.axis('off')
 # plt.show()
 plt.savefig('test_bluethroat.jpg')
 
-# How I want to call it
-rotatedRectWithMaxArea 
-augmenter = CageAugmenter(padding)
-img = augmenter (img)
-return img
 
 
--- ct_classifier/caged_augmenter_class.py
+++ ct_classifier/caged_augmenter_class.py
@@ -7,44 +7,44 @@ import random
 from torchvision.transforms import functional as F
 from torchvision.transforms import Pad
 
-class CageAugmenter:
-    def __init__(self):
+class CageAugmenter: 
+    def __init__(self): # what stays fixed
         self.resolution = [1000, 1000]
         self.num_bars_x = 4
         self.num_bars_y = 6
         self.kernel = [30, 30]
         self.color_alpha = 0.3
-        
-    def rotatedRectWithMaxArea(self, w, h, angle):
-"""
-Given a rectangle of size wxh that has been rotated by 'angle' (in
-radians), computes the width and height of the largest possible
-axis-aligned rectangle (maximal area) within the rotated rectangle.
-"""
-    if w <= 0 or h <= 0:
-        return 0,0
+    
+    def __call__(self, img): # def rotatedRectWithMaxArea(self, w, h, angle):
+        """
+        Given a rectangle of size wxh that has been rotated by 'angle' (in
+        radians), computes the width and height of the largest possible
+        axis-aligned rectangle (maximal area) within the rotated rectangle.
+        """
+        if w <= 0 or h <= 0:
+            return 0,0
 
-    width_is_longer = w >= h
-    side_long, side_short = (w,h) if width_is_longer else (h,w)
+        width_is_longer = w >= h
+        side_long, side_short = (w,h) if width_is_longer else (h,w)
 
-    # since the solutions for angle, -angle and 180-angle are all the same,
-    # if suffices to look at the first quadrant and the absolute values of sin,cos:
-    sin_a, cos_a = abs(math.sin(angle)), abs(math.cos(angle))
-    if side_short <= 2.*sin_a*cos_a*side_long or abs(sin_a-cos_a) < 1e-10:
-        # half constrained case: two crop corners touch the longer side,
-        #   the other two corners are on the mid-line parallel to the longer line
-        x = 0.5*side_short
-        wr,hr = (x/sin_a,x/cos_a) if width_is_longer else (x/cos_a,x/sin_a)
-    else:
-        # fully constrained case: crop touches all 4 sides
-        cos_2a = cos_a*cos_a - sin_a*sin_a
-        wr,hr = (w*cos_a - h*sin_a)/cos_2a, (h*cos_a - w*sin_a)/cos_2a
+        # since the solutions for angle, -angle and 180-angle are all the same,
+        # if suffices to look at the first quadrant and the absolute values of sin,cos:
+        sin_a, cos_a = abs(math.sin(angle)), abs(math.cos(angle))
+        if side_short <= 2.*sin_a*cos_a*side_long or abs(sin_a-cos_a) < 1e-10:
+            # half constrained case: two crop corners touch the longer side,
+            #   the other two corners are on the mid-line parallel to the longer line
+            x = 0.5*side_short
+            wr,hr = (x/sin_a,x/cos_a) if width_is_longer else (x/cos_a,x/sin_a)
+        else:
+            # fully constrained case: crop touches all 4 sides
+            cos_2a = cos_a*cos_a - sin_a*sin_a
+            wr,hr = (w*cos_a - h*sin_a)/cos_2a, (h*cos_a - w*sin_a)/cos_2a
 
-    return int(wr),int(hr)
+        return int(wr),int(hr)
+    
 
     def apply(self, im):
         self.im = im
-
         # Parameters that you randomize
         self.bar_radius_range = [10, 30]
         self.bar_radius = random.uniform(*self.bar_radius_range)
@@ -55,4 +55,8 @@ axis-aligned rectangle (maximal area) within the rotated rectangle.
         self.rotate_range = [-20, 20]
         self.rotate = math.radians(random.uniform(*self.rotate_range))
 
-        return transformed_image
+            # How I want to call it
+    rotatedRectWithMaxArea 
+    augmenter = CageAugmenter(padding)
+    img = augmenter (img)
+    return img ?
