af5895561602cd45a205d359d943175687c7e9af
sicily <sicilyfiennes@gmail.com>
2023-09-01 16:26:34 +0000

    Better prediction plot

--- pres_plots.py
+++ pres_plots.py
@@ -1,15 +1,8 @@
-# Plotting the augmentations
-
 import matplotlib.pyplot as plt
-# After 50 epochs 
-
-# show what the defaults were for those augmentations 
+import numpy as np
 
 # Example mAP values (replace these with your actual data)
-methods = ['No Aug Unbalanced', 'Blur Unbalanced', 'Horizontal Flipping Unbalanced', 'Random Sharpness Unbalanced', 'All Aug Unbalanced', 'All Aug Umpsampled']
-# method A BLUR: train = 0.9935017680370775, val =0.7645522951549639
-# flipping : train = 0.9803945972730336, val = 0.8155913197540258
-# sharpness: train = 0.9957616060035979, val = 0.7625355412613961
+methods = ['No Aug Unbalanced', 'Blur Unbalanced', 'Horizontal Flipping Unbalanced', 'Random Sharpness Unbalanced', 'All Aug Unbalanced', 'All Aug Upsampled']
 train_mAP = [1.0, 0.99, 0.98, 1.0, 0.97, 0.97]  # mAP values during training
 val_mAP = [0.77, 0.76, 0.82, 0.76, 0.77, 0.79]    # mAP values during validation
 
@@ -17,25 +10,29 @@ val_mAP = [0.77, 0.76, 0.82, 0.76, 0.77, 0.79]    # mAP values during validation
 fig, ax = plt.subplots(figsize=(10, 6))
 
 bar_width = 0.35
-index = range(len(methods))
+index = np.arange(len(methods))
+
+# Calculate the x-coordinates for both training and validation bars
+combined_x = index + bar_width / 2
 
 # Bar plots for training and validation mAP
-train_bars = ax.bar(index, train_mAP, bar_width, label='Training mAP')
-val_bars = ax.bar([i + bar_width for i in index], val_mAP, bar_width, label='Validation mAP')
+train_bars = ax.bar(combined_x, train_mAP, bar_width, label='Training mAP')
+val_bars = ax.bar(combined_x, val_mAP, bar_width, label='Validation mAP', bottom=train_mAP)  # Stack validation bars on top of training bars
 
 ax.set_xlabel('Augmentation Methods')
 ax.set_ylabel('Mean Average Precision (mAP)')
 ax.set_title('Mean Average Precision during Training and Validation')
-ax.set_xticks([i + bar_width/2 for i in index])
-ax.set_xticklabels(methods, rotation=45)  # Rotate x-axis labels
-
-# ax.set_xticklabels(methods)
-# ax.legend()
+ax.set_xticks([])  # Remove x-axis ticks
+ax.set_xticklabels([])  # Remove x-axis labels
 
 # Place the legend to the right of the plot
 ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
+# Add labels in the middle of each bar
+for i, label in enumerate(methods):
+    ax.text(combined_x[i], (train_mAP[i] + val_mAP[i]) / 2, label, ha='center', va='bottom', rotation=45)
+
 # Display the plot
 plt.tight_layout()
-# plt.tight_layout(rect=[0, 0, 0.85, 1])
-plt.savefig('mAP_augmentations new.png')
+plt.savefig('mAP_augmentations_new.png')
+plt.show()
